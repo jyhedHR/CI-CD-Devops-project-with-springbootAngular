25dd550fd6a7af99f80d2cf3db194f55eab4cea3
yasminebouteraa <bouteraa.yassmine@esprit.tn>
2025-02-26 22:54:04 +0100

    fix dockerfile

--- src/test/java/tn/esprit/spring/services/IPisteServicesTest.java
+++ src/test/java/tn/esprit/spring/services/IPisteServicesTest.java
@@ -2,37 +2,77 @@ package tn.esprit.spring.services;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.spring.entities.Piste;
 import tn.esprit.spring.repositories.IPisteRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
-class IPisteServicesTest {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PisteServicesImplTest {
+
     @InjectMocks
     private PisteServicesImpl pisteServices;
-    @Mock
 
+    @Mock
     private IPisteRepository pisteRepository;
 
+    private Piste piste;
+
     @BeforeEach
-    public void setUp() {
+    void setUp() {
+        piste = new Piste();
+        piste.setNumPiste(1L);
 
     }
 
     @Test
     void retrieveAllPistes() {
+        List<Piste> pistes = Arrays.asList(piste, new Piste());
+        when(pisteRepository.findAll()).thenReturn(pistes);
+
+        List<Piste> result = pisteServices.retrieveAllPistes();
+
+        assertEquals(2, result.size());
+        verify(pisteRepository, times(1)).findAll();
     }
 
     @Test
     void addPiste() {
+        when(pisteRepository.save(any(Piste.class))).thenReturn(piste);
+
+        Piste savedPiste = pisteServices.addPiste(piste);
+
+        assertNotNull(savedPiste);
+        assertEquals(piste.getNumPiste(), savedPiste.getNumPiste());
+        verify(pisteRepository, times(1)).save(piste);
     }
 
     @Test
     void removePiste() {
+        doNothing().when(pisteRepository).deleteById(piste.getNumPiste());
+
+        pisteServices.removePiste(piste.getNumPiste());
+
+        verify(pisteRepository, times(1)).deleteById(piste.getNumPiste());
     }
 
     @Test
     void retrievePiste() {
+        when(pisteRepository.findById(piste.getNumPiste())).thenReturn(Optional.of(piste));
+
+        Piste foundPiste = pisteServices.retrievePiste(piste.getNumPiste());
+
+        assertNotNull(foundPiste);
+        assertEquals(piste.getNumPiste(), foundPiste.getNumPiste());
+        verify(pisteRepository, times(1)).findById(piste.getNumPiste());
     }
 }
