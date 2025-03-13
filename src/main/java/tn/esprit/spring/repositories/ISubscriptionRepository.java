f2df5ea2f1d9e2b048a95346da50153414ad4d07
Eya <eya.nehdi@esprit.tn>
2025-03-13 20:14:21 +0100

    fixin

--- src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
@@ -18,8 +18,8 @@ public interface ISubscriptionRepository extends CrudRepository<Subscription, Lo
 
     List<Subscription> getSubscriptionsByStartDateBetween(LocalDate date1, LocalDate date2);
 
-    @Query("select distinct s from Subscription s where s.endDate <= CURRENT_TIME order by s.endDate")
-    List<Subscription> findDistinctOrderByEndDateAsc();
+  //  @Query("select distinct s from Subscription s where s.endDate <= CURRENT_TIME order by s.endDate")
+   // List<Subscription> findDistinctOrderByEndDateAsc();
 
 
     //@Query("select (sum(s.price))/(count(s)) from Subscription s where s.typeSub = ?1")
