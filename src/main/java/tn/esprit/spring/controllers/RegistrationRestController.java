901c4d3b2aed69de7373eccbeba486069f6f5699
Eya <eya.nehdi@esprit.tn>
2025-04-18 00:29:18 +0100

    registration problem

--- src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
+++ src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
@@ -4,17 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.spring.entities.Instructor;
 import tn.esprit.spring.entities.Registration;
 import tn.esprit.spring.entities.Support;
-import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.services.IRegistrationServices;
 
 import java.util.List;
 
 @Tag(name = "\uD83D\uDDD3️Registration Management")
 @RestController
-@RequestMapping("/reg")
+@RequestMapping("/registration")
 @RequiredArgsConstructor
 public class RegistrationRestController {
     private final IRegistrationServices registrationServices;
